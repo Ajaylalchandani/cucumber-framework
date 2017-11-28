6d34deb901ed1e6e9ff7aa5bcbb89ea6923feb4b
Ajay Lal Chandani <alalchandani@coca-cola.com>
2017-11-28 15:22:14 -0500

    New Changes

---
+++ automationframework/src/main/java/stepDefinition/TestSteps.java
@@ -0,0 +1,48 @@
+package stepDefinition;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class TestSteps {
+
+	@Given("^User is on Home Page$")
+	public void User_is_on_Home_Page() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@When("^User Navigate to LogIn Page$")
+	public void User_Navigate_to_LogIn_Page() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@When("^User enters UserName and Password$")
+	public void User_enters_UserName_and_Password() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Then("^Message displayed Login Successfully$")
+	public void Message_displayed_Login_Successfully() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+	
+	
+	@When("^User LogOut from the Application$")
+	public void User_LogOut_from_the_Application() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Then("^Message displayed LogOut Successfully$")
+	public void Message_displayed_LogOut_Successfully() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+
+}
